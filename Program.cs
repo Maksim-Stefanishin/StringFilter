3a6c92033d9bde181ae27a3759adf2950423d4dd
Maksim-Stefanishin <stefanishin-91@mail.ru>
2023-07-30 00:48:04 +0300

    StringFilter

--- Program.cs
+++ Program.cs
@@ -2,4 +2,39 @@
 //  Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 //  При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
+    string[] FilterStringArray(string[] inputArray)
+    {
+        int count = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                count++;
+            }
+        }
 
+        string[] filteredArray = new string[count];
+        int index = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                filteredArray[index++] = inputArray[i];
+            }
+        }
+
+        return filteredArray;
+    }	
+
+    Console.WriteLine("Введите элементы массива, разделяя их запятой:");
+    string? input = Console.ReadLine();
+
+    string[] stringArray = input.Split(',');
+
+    string[] filteredArray = FilterStringArray(stringArray);
+
+    Console.WriteLine("Отфильтрованный массив:");
+    Console.WriteLine(string.Join(", ", filteredArray));
+    
+
+    
